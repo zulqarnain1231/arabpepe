0c844a969dc9cadc768b4fe7acc8aeb6b6dad5c7
zulqarnain1231 <zulqarnainniazi447@gmail.com>
2023-07-20 12:56:47 -0700

    added alll the assets and dummy data

--- components/shared/Wrapper.jsx
+++ components/shared/Wrapper.jsx
@@ -1,12 +1,6 @@
 import React from "react";
 
-interface Props {
-  children: React.ReactNode;
-  id: string;
-  style?: string;
-}
-
-const Wrapper: React.FC<Props> = ({ children, style, id }) => {
+const Wrapper = ({ children, style, id }) => {
   return (
     <section id={id} className={`w-full ${style}`}>
       <div className="w-full h-full max-w-[1280px] mx-auto md:px-8 px-4">
