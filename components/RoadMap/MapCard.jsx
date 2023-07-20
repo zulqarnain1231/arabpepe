14d16a81a6e2321bb077210beae2227b10e91542
zulqarnain1231 <zulqarnainniazi447@gmail.com>
2023-07-20 12:55:16 -0700

    finished roadmap section

---
+++ components/RoadMap/MapCard.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Text from "../shared/Typography/Text";
+import { TbSquareRoundedChevronRightFilled } from "react-icons/tb";
+
+
+const MapCard = ({ options, title }) => {
+  return (
+    <div className="h-[280px]  border-2 rounded-tr-[40px] rounded-bl-[40px] border-white-main p-6 m-auto">
+      <div className="h-full w-full flex flex-col items-start justify-start gap-2 border-2 border-white-main rounded-tr-[40px] rounded-bl-[40px] p-2">
+        <p className="font-[500] text-white-main text-[18px]">{title}</p>
+        <div className="w-full flex flex-col gap-2 items-start justify-start">
+          {options.map((item, index) => {
+            return (
+              <div
+                key={index}
+                className="w-full flex items-center justify-start gap-2"
+              >
+                <TbSquareRoundedChevronRightFilled className="text-white-main" />
+                <Text text={item} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MapCard;
---
+++ components/RoadMap/RoadMap.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import Wrapper from "../shared/Wrapper";
+import Heading from "../shared/Typography/Heading";
+import * as Icons from "../../constants/Svg/Icons";
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineDot from "@mui/lab/TimelineDot";
+import MapCard from "./MapCard";
+import Data from "../../constants/Json/Dummy.json";
+
+const RoadMap = () => {
+  return (
+    <Wrapper id="roadMap" style="py-16">
+      <div className="w-full flex flex-col lg:items-center items-start justify-start gap-10">
+        <Heading text="Road Map" />
+        <div className="w-full">
+          <Timeline position="alternate">
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot />
+                <TimelineConnector className="h-[300px]" />
+              </TimelineSeparator>
+              <MapCard
+                title="Phase 1: Token Strategy & Partnerships (Month 1-2)"
+                options={Data.roadMap.phase1}
+              />
+            </TimelineItem>
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot />
+                <TimelineConnector className="h-[300px]" />
+              </TimelineSeparator>
+              <MapCard
+                title="Phase 2: Digital Presence & Listings (Month 1-2)"
+                options={Data.roadMap.phase2}
+              />
+            </TimelineItem>
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot />
+                <TimelineConnector className="h-[300px]" />
+              </TimelineSeparator>
+              <TimelineContent>
+                <MapCard
+                  title="Phase 3: Community & Market Growth (Month 1-2)"
+                  options={Data.roadMap.phase3}
+                />
+              </TimelineContent>
+            </TimelineItem>
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot />
+              </TimelineSeparator>
+              <MapCard
+                title="Phase 4: Exchange Listing & Brand Expansion (Month 3)"
+                options={Data.roadMap.phase4}
+              />
+            </TimelineItem>
+          </Timeline>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default RoadMap;
